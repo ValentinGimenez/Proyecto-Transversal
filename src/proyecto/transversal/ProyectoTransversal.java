176d0c0365f28845e7d7006b05db5d6f513350a5
GonzaGalfre <gonzaa.galfre@gmail.com>
2023-05-23 17:57:17 -0300

    feat: test saveMateria, saveInscripcion

--- src/proyecto/transversal/ProyectoTransversal.java
+++ src/proyecto/transversal/ProyectoTransversal.java
@@ -7,7 +7,11 @@ package proyecto.transversal;
 
 import Control.Conexion;
 import Control.DataAlumno;
+import Control.DataInscripcion;
+import Control.DataMateria;
 import Modelo.Alumno;
+import Modelo.Materia;
+import java.io.IOException;
 import java.time.LocalDate;
 
 /**
@@ -22,13 +26,30 @@ public class ProyectoTransversal {
     public static void main(String[] args) {
         //Prueba de conexión
         Conexion.getConexion();
+        DataMateria dataMateria = new DataMateria();
+        DataAlumno dataAlumno1 = new DataAlumno();
+        DataInscripcion dataInscripcion = new DataInscripcion();
         //Agregar alumno
         //Alumno alumno1 = new Alumno (55555555, "Perez", "Juan", LocalDate.of(1999, 5, 15), false);
-        DataAlumno dataAlumno1 = new DataAlumno();
+        
         //dataAlumno1.guardarAlumno(alumno1);
+        
         dataAlumno1.activarAlumno(1);
         System.out.println(dataAlumno1.buscarAlumno(1));
         System.out.println(dataAlumno1.buscarAlumnoPorDni(22222222));
+        
+        Materia materia = new Materia("Matematica", 2, true);
+        Alumno alumno2 = new Alumno(123, "Rodriguez", "Carlos", LocalDate.of(2001, 8, 21), true);
+        
+        dataMateria.guardarMateria(materia);
+        
+        try {
+            dataInscripcion.insertInscripcion(alumno2, materia);
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+        
+        
     }
     
 }
